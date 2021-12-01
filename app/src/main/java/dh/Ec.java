bd38ac04c9dd0ba89df83a0958687bde9efa8e4c
snowmerak <snowmerak@outlook.com>
2021-12-01 14:38:49 +0900

    add key backup

--- app/src/main/java/dh/Ec.java
+++ app/src/main/java/dh/Ec.java
@@ -7,14 +7,17 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.ECFieldFp;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPrivateKeySpec;
 import java.security.spec.EllipticCurve;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -24,14 +27,17 @@ import convert.Hex;
 
 public class Ec {
 
-    private KeyPair keyPair;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
 
     public Ec() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchProviderException {
         ECGenParameterSpec ecParamSpec = new ECGenParameterSpec("secp521r1");
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
         kpg.initialize(ecParamSpec);
 
-        keyPair = kpg.generateKeyPair();
+        KeyPair keyPair = kpg.generateKeyPair();
+        privateKey = keyPair.getPrivate();
+        publicKey = keyPair.getPublic();
     }
 
     public String share(String other) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
@@ -43,13 +49,36 @@ public class Ec {
         PublicKey pubKey = kf.generatePublic(x509ks);
 
         KeyAgreement aKA = KeyAgreement.getInstance("ECDH");
-        aKA.init(keyPair.getPrivate());
+        aKA.init(privateKey);
         aKA.doPhase(pubKey, true);
 
         return Hex.from(aKA.generateSecret());
     }
 
     public String getPublicKey() {
-        return Hex.from(keyPair.getPublic().getEncoded());
+        return Hex.from(publicKey.getEncoded());
+    }
+
+    public void setPublicKey(String encodedHex) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] encoded = Hex.toBytes(encodedHex);
+
+        KeyFactory kf = KeyFactory.getInstance("EC");
+
+        X509EncodedKeySpec x509ks = new X509EncodedKeySpec(encoded);
+        publicKey = kf.generatePublic(x509ks);
+    }
+
+    public String getPrivateKey() {
+        return Hex.from(privateKey.getEncoded());
+    }
+
+    public void setPrivateKey(String encodedHex) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] encoded = Hex.toBytes(encodedHex);
+
+        KeyFactory kf = KeyFactory.getInstance("EC");
+
+        PKCS8EncodedKeySpec x509ks = new PKCS8EncodedKeySpec(encoded);
+
+        privateKey = kf.generatePrivate(x509ks);
     }
 }
--- app/src/main/java/org/astraea/sirius/HandshakeActivity.java
+++ app/src/main/java/org/astraea/sirius/HandshakeActivity.java
@@ -15,11 +15,18 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 
 import dh.Ec;
+import io.paperdb.Book;
+import io.paperdb.Paper;
 
 public class HandshakeActivity extends AppCompatActivity {
+    private final String PRIVATEKEY = "PRIVATEKEY", PUBLICKEY = "PUBLICKEY", HANDSHAKE = "HANDSHAKE";
+
+
     private Ec ec;
 
     @Override
@@ -28,7 +35,42 @@ public class HandshakeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_handshake);
         setTitle("Handshake");
 
+        Book book = Paper.book(HANDSHAKE);
+
         TextView publicKeyView = findViewById(R.id.publicKeyView);
+
+        try {
+            ec = new Ec();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "cannot init", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return;
+        }
+
+        String privateKey = book.read(PRIVATEKEY);
+        if (privateKey != null) {
+            try {
+                ec.setPrivateKey(privateKey);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "cannot set previous private key", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String publicKey = book.read(PUBLICKEY);
+            if (publicKey != null) {
+                try {
+                    ec.setPublicKey(publicKey);
+                    Toast.makeText(getApplicationContext(), "loaded previous key pair", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "cannot set previous public key", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        }
+        book.write(PRIVATEKEY, ec.getPrivateKey());
+        book.write(PUBLICKEY, ec.getPublicKey());
+        publicKeyView.setText(ec.getPublicKey());
 
         Button resetButton = findViewById(R.id.resetButton);
         resetButton.setOnClickListener((ev) -> {
@@ -40,6 +82,9 @@ public class HandshakeActivity extends AppCompatActivity {
                 return;
             }
             publicKeyView.setText(ec.getPublicKey());
+
+            book.write(PRIVATEKEY, ec.getPrivateKey());
+            book.write(PUBLICKEY, ec.getPublicKey());
         });
 
         TextView passwordView = findViewById(R.id.passwordView);
